6aa915d8e10e3ab126514671e0eb2597f04d37fe
Mateusz <mateusz.harlos@gmail.com>
2020-11-04 17:04:25 +0000

    Extensions finished

---
+++ console.py
@@ -0,0 +1,25 @@
+import pdb
+from models.book import Book
+from models.author import Author
+
+import repositories.book_repository as book_repository
+import repositories.author_repository as author_repository
+
+book_repository.delete_all()
+author_repository.delete_all()
+
+author1 = Author("Suzanne", "Collins")
+author_repository.save(author1)
+
+author2 = Author("J.R.R.", "Tolkien")
+author_repository.save(author2)
+
+book1 = Book("The Hunger Games", "Young Adult", "Scholastic", author1)
+book_repository.save(book1)
+
+book2 = Book("The Lord of the Rings", "Fantasy", "Harper Collins", author2)
+book_repository.save(book2)
+book3 = Book("The Hobbit", "Fantasy", "Harper Collins", author2)
+book_repository.save(book3)
+
+pdb.set_trace()
---
+++ controllers/books_controller.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.book import Book
+import repositories.book_repository as book_repository
+import repositories.author_repository as author_repository
+
+books_blueprint = Blueprint("books", __name__)
+
+
+# ----- MVP -----
+
+# INDEX
+@books_blueprint.route("/books")
+def books():
+    books = book_repository.select_all()
+    return render_template("books/index.html", all_books=books)
+
+# DELETE
+@books_blueprint.route("/books/<id>/delete", methods=["POST"])
+def delete_book(id):
+    book_repository.delete(id)
+    return redirect('/books')
+
+# ----- EXTENSIONS -----
+
+# SHOW
+@books_blueprint.route("/books/<id>", methods=["GET"])
+def show_book(id):
+    book = book_repository.select(id)
+    return render_template("/books/show.html", book=book)
+
+# NEW
+@books_blueprint.route("/books/new", methods=["GET"])
+def new_book():
+    authors = author_repository.select_all()
+    return render_template("/books/new.html", all_authors=authors)
+
+# CREATE
+@books_blueprint.route("/books", methods=["POST"])
+def create_book():
+    author_id = request.form['author_id'] 
+    title = request.form['title']
+    genre = request.form['genre']
+    publisher = request.form['publisher']
+
+    author = author_repository.select(author_id)
+    book = Book(title, genre, publisher, author)
+    book_repository.save(book)
+    return redirect("/books")
+
+
+# ----- ADVANCED EXTENSIONS -----
+
+# EDIT
+
+
+# UPDATE
+
+
+
+
